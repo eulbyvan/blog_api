dfd9f85dbdf1c1263842899b0b66e55ae3b6be85
eulbyvan <work.issdq@gmail.com>
2024-05-25 07:48:06 +0700

    add update for posts

--- internal/repository/post_repository.go
+++ internal/repository/post_repository.go
@@ -84,8 +84,96 @@ func (r *postRepository) Create(post *entity.Post) (*entity.Post, error) {
 }
 
 func (r *postRepository) Update(post *entity.Post) (*entity.Post, error) {
-	// TODO implement me!
-	return nil, nil
+	tx, err := r.db.Begin()
+	if err != nil {
+		return nil, err
+	}
+
+	stmt, err := tx.Prepare(`UPDATE posts SET title = $1, content = $2, status = $3, publish_date = $4 WHERE id = $5`)
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(post.Title, post.Content, post.Status, post.PublishDate, post.ID)
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	// fetch existing tags
+	existingTags := make(map[string]int)
+	rows, err := tx.Query(`SELECT t.id, t.label FROM tags t JOIN post_tags pt ON t.id = pt.tag_id WHERE pt.post_id = $1`, post.ID)
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var tag entity.Tag
+		err := rows.Scan(&tag.ID, &tag.Label)
+		if err != nil {
+			tx.Rollback()
+			return nil, err
+		}
+		existingTags[tag.Label] = tag.ID
+	}
+
+	// process each tag in the updated post
+	for i, tag := range post.Tags {
+		var tagID int
+		if existingID, exists := existingTags[tag.Label]; exists {
+			tagID = existingID
+		} else {
+			err = tx.QueryRow(`SELECT id FROM tags WHERE label = $1`, tag.Label).Scan(&tagID)
+			if err != nil {
+				if err == sql.ErrNoRows {
+					err = tx.QueryRow(`INSERT INTO tags (label) VALUES ($1) RETURNING id`, tag.Label).Scan(&tagID)
+					if err != nil {
+						tx.Rollback()
+						return nil, err
+					}
+				} else {
+					tx.Rollback()
+					return nil, err
+				}
+			}
+		}
+		post.Tags[i].ID = tagID
+
+		_, err = tx.Exec(`INSERT INTO post_tags (post_id, tag_id) VALUES ($1, $2) ON CONFLICT DO NOTHING`, post.ID, tagID)
+		if err != nil {
+			tx.Rollback()
+			return nil, err
+		}
+	}
+
+	// remove tags that are no longer associated with the post
+	for label, id := range existingTags {
+		found := false
+		for _, tag := range post.Tags {
+			if tag.Label == label {
+				found = true
+				break
+			}
+		}
+		if !found {
+			_, err = tx.Exec(`DELETE FROM post_tags WHERE post_id = $1 AND tag_id = $2`, post.ID, id)
+			if err != nil {
+				tx.Rollback()
+				return nil, err
+			}
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return nil, err
+	}
+
+	return post, nil
 }
 
 func (r *postRepository) Delete(id int) error {
